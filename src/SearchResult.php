145236a9c3529fbde22ab6af400901c2502a6ea5
Roxayl <romuald.fabiani@outlook.fr>
2021-04-03 01:23:55 +0200

    Creates new properties in search results

--- src/SearchResult.php
+++ src/SearchResult.php
@@ -13,22 +13,28 @@ class SearchResult
     /** @var null|string */
     public $url;
 
+    /** @var null|string */
+    public $context;
+
+    /** @var null|string */
+    public $description;
+
     /** @var string */
     public $type;
 
-    public function __construct(Searchable $searchable, string $title, ?string $url = null)
+    public function __construct(Searchable $searchable, string $title, ?string $context = null,
+                                ?string $description = null, ?string $url = null)
     {
         $this->searchable = $searchable;
-
         $this->title = $title;
-
+        $this->context = $context;
+        $this->description = $description;
         $this->url = $url;
     }
 
     public function setType(string $type): self
     {
         $this->type = $type;
-
         return $this;
     }
 }
